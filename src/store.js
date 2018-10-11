350bb00a268c333f3f3eba57b4060977923ffd37
Nikolas Melgarejo <nikoclops@gmail.com>
2018-10-11 14:38:20 -0400

    Fixes createstore import

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import cheeseReducer from "./reducers/cheese";
-import { createStore } from "../../../../AppData/Local/Microsoft/TypeScript/3.0/node_modules/redux";
+import { createStore } from "redux";
 import thunk from "redux-thunk";
 import { applyMiddleware, compose } from "redux";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
