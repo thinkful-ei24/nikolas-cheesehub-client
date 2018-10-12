74ca170ce9d26166741a923d55344e6557a50182
Nikolas Melgarejo <nikoclops@gmail.com>
2018-10-12 10:42:26 -0400

    Slight fix

--- src/actions/cheese.js
+++ src/actions/cheese.js
@@ -21,12 +21,8 @@ export const fetchCheesesRequest = value => {
 
 export const getCheesesActionAsync = value => dispatch => {
 	dispatch(fetchCheesesRequest());
-	return fetch("https://cheesehubserver.herokuapp.com/api/cheeses")
-		.then(res => {
-			return res.json();
-		})
-		.then(res => {
-			return dispatch(fetchCheesesSuccess(res));
-		})
+	fetch("https://cheesehubserver.herokuapp.com/api/cheeses")
+		.then(res => res.json())
+		.then(res => dispatch(fetchCheesesSuccess(res)))
 		.catch(err => dispatch(fetchCheesesError(err)));
 };
